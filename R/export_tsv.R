a466240cc8a543e3f9d79e0360ad494e029482e4
zhaoy <zhaoy@users.noreply.github.com>
2017-06-08 18:48:28 -0400

    add export_tsv

---
+++ R/export_tsv.R
@@ -0,0 +1,29 @@
+#' Export tab-separated-values (tsv) files
+#'
+#' Execute readr::write_tsv with assumptions about values of some arguments.
+#'
+#' Only blank cells represent missing data.
+#' Over-write existing file if one exists.
+#' Write column names at top of file.
+#'
+#' @param tbl A table.
+#' @param path A path.
+#'
+#' @return
+#'
+#' @keywords
+#'
+#' @export
+#'
+#' @examples
+
+export_tsv <- function(tbl,
+                       path) {
+
+    write_tsv(x = tbl,
+              path = path,
+              na = "",
+              append = FALSE,
+              col_names = TRUE)
+
+}
