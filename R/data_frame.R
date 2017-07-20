1488c7533508d920a802b047111ecb7b03ef8f25
zhaoy <zhaoy@users.noreply.github.com>
2017-07-20 19:00:36 -0400

    delete data_frame.R

--- R/data_frame.R
+++
@@ -1,8 +0,0 @@
-data_frame <- function(x) {
-  data.frame(x,
-             row.names = NULL,
-             check.rows = TRUE,
-             check.names = TRUE,
-             fix.empty.names = TRUE,
-             stringsAsFactors = FALSE)
-}
