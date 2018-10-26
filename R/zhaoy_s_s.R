ef7d20fd3686c140575199bdf1d80932aaf6d78f
zhaoy <zhaoy@users.noreply.github.com>
2018-10-26 00:43:33 -0400

    edit

--- R/zhaoy_s_s.R
+++ R/zhaoy_s_s.R
@@ -13,7 +13,7 @@
 #' A length-one character vector.
 #'
 #' If \code{x} is not a numeric or date / date-time / time-interval object,
-#' and the statistic is "min" or "max",
+#' and the statistic is minimum or maximum,
 #' \code{\link{NA}} is returned.
 #'
 #' If \code{x} is a date / date-time / time-interval object,
