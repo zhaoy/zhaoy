eb42a3d289f86300700e9c1bd5f93e7086fca57d
zhaoy <zhaoy@users.noreply.github.com>
2019-07-24 01:04:05 -0400

    edit

--- R/tidy_activity.R
+++ R/tidy_activity.R
@@ -11,8 +11,9 @@
 #' @return
 #' A tibble.
 #'
-#' @importFrom dplyr case_when filter select
-#' @importFrom purrr map
+#' @importFrom dplyr case_when filter mutate select
+#' @importFrom lubridate as_date
+#' @importFrom purrr map_chr
 #' @importFrom tidyr fill
 #'
 #' @export
@@ -20,138 +21,139 @@
 tidy_activity <- function(x) {
 
   activity <- x
-
+  
   names(x = activity) <- c("x_1",
                            "x_2",
                            "x_3",
                            "x_4",
                            "x_5",
                            "x_6")
-
+  
   x_1_pattern <- "(musc - adult hiv clinic)|count:|totals|(print date:)"
-
+  
   activity <- dplyr::filter(.data = activity,
                             grepl(pattern = x_1_pattern,
                                   x = activity$x_1,
                                   ignore.case = TRUE,
                                   fixed = FALSE) == FALSE)
-
+  
   activity <- dplyr::select(.data = activity,
-                            -c(x_4,
-                               x_6))
-
-# provider_name
-
-  activity$provider_name <- dplyr::case_when(grepl(pattern = "provider:",
-                                                   x = activity$x_1,
-                                                   fixed = TRUE) == TRUE ~
-                                             activity$x_1,
-                                             grepl(pattern = "provider:",
-                                                   x = activity$x_1,
-                                                   fixed = TRUE) == FALSE ~
-                                             NA_character_)
-
-  activity$provider_name[is.na(x = activity$provider_name) == FALSE] <- strsplit(x = activity$provider_name[is.na(x = activity$provider_name) == FALSE],
-                                                                                 split = ":\\s{2}",
-                                                                                 fixed = FALSE)
-
-  activity$provider_name[is.na(x = activity$provider_name) == FALSE] <- purrr::map(.x = activity$provider_name[is.na(x = activity$provider_name) == FALSE],
-                                                                                   .f = 2)
-
-  activity$provider_name <- c(activity$provider_name,
-                              recursive = TRUE)
-
-  activity$provider_name <- gsub(pattern = "/musc/scgov",
-                                 replacement = "",
-                                 x = activity$provider_name,
-                                 ignore.case = TRUE,
-                                 fixed = FALSE)
-
-  activity$provider_name <- gsub(pattern = "\\s{2,}",
-                                 replacement = " ",
-                                 x = activity$provider_name,
-                                 ignore.case = TRUE,
-                                 fixed = FALSE)
-
+                            x_1,
+                            x_2,
+                            x_3,
+                            x_5)
+  
+  # activity_provider
+  
+  activity <- dplyr::mutate(.data = activity,
+                            activity_provider = dplyr::case_when(grepl(pattern = "provider:",
+                                                                       x = activity$x_1,
+                                                                       ignore.case = TRUE,
+                                                                       fixed = FALSE) == TRUE ~
+                                                                 activity$x_1,
+                                                                 grepl(pattern = "provider:",
+                                                                       x = activity$x_1,
+                                                                       ignore.case = TRUE,
+                                                                       fixed = FALSE) == FALSE ~
+                                                                 NA_character_))
+  
+  activity$activity_provider[is.na(x = activity$activity_provider) == FALSE] <- activity$activity_provider %>%
+    subset(subset = is.na(x = activity$activity_provider) == FALSE) %>%
+    strsplit(split = ":\\s{2}",
+             fixed = FALSE) %>%
+    purrr::map_chr(.f = 2)
+  
+  activity$activity_provider <- gsub(pattern = "\\s{2,}",
+                                     replacement = " ",
+                                     x = activity$activity_provider,
+                                     ignore.case = TRUE,
+                                     fixed = FALSE)
+  
+  activity$activity_provider <- gsub(pattern = "/musc/scgov",
+                                     replacement = "",
+                                     x = activity$activity_provider,
+                                     ignore.case = TRUE,
+                                     fixed = FALSE)
+  
   activity <- tidyr::fill(data = activity,
-                          provider_name,
+                          activity_provider,
                           .direction = "down")
-
+  
   activity <- dplyr::filter(.data = activity,
                             grepl(pattern = "provider:",
                                   x = activity$x_1,
-                                  fixed = TRUE) == FALSE)
-
-# type
-
-  type <- unique(x = activity$x_1[activity$x_2 == "service type"],
-                 incomparables = FALSE)
-
-  activity$type <- dplyr::case_when(activity$x_1 %in% type == TRUE ~
-                                    activity$x_1,
-                                    activity$x_1 %in% type == FALSE ~
-                                    NA_character_)
-
-  activity <- tidyr::fill(data = activity,
-                          type,
-                          .direction = "down")
-
-  activity <- dplyr::filter(.data = activity,
-                            x_2 != "service type")
-
-# mrn
-
+                                  ignore.case = TRUE,
+                                  fixed = FALSE) == FALSE)
+  
+  # mrn
+  
   activity$mrn <- dplyr::case_when(activity$x_2 == "client id:" ~
                                    activity$x_3,
                                    activity$x_2 != "client id:" ~
                                    NA_character_)
-
+  
   activity <- tidyr::fill(data = activity,
                           mrn,
-                         .direction = "down")
-
+                          .direction = "down")
+  
   activity$mrn <- zhaoy::lz_id(x = activity$mrn,
                                lz = TRUE)
-
-# pp_ship
-
+  
+  # pp_ship
+  
   activity$pp_ship <- dplyr::case_when(activity$x_2 == "client id:" ~
                                        activity$x_5,
                                        activity$x_2 != "client id:" ~
                                        NA_character_)
-
+  
   activity <- tidyr::fill(data = activity,
                           pp_ship,
                           .direction = "down")
-
+  
   activity <- dplyr::filter(.data = activity,
                             x_2 != "client id:")
-
+  
   activity <- dplyr::select(.data = activity,
-                            -c(x_3,
-                               x_5))
-
-# summary
-
-  names(x = activity)[names(x = activity) == "x_1"] <- "summary"
-
-# activity_date
-
+                            x_1,
+                            x_2,
+                            activity_provider,
+                            mrn,
+                            pp_ship)
+  
+  # activity_type
+  
+  x_1_pattern <- unique(x = activity$x_1[activity$x_2 == "service type"],
+                        incomparables = FALSE)
+  
+  activity$activity_type <- dplyr::case_when(activity$x_1 %in% x_1_pattern == TRUE ~
+                                             activity$x_1,
+                                             activity$x_1 %in% x_1_pattern == FALSE ~
+                                             NA_character_)
+  
+  activity <- tidyr::fill(data = activity,
+                          activity_type,
+                          .direction = "down")
+  
+  activity <- dplyr::filter(.data = activity,
+                            x_2 != "service type")
+  
+  # activity_date
+  
   activity$x_2 <- as.numeric(x = activity$x_2)
-
-  activity$x_2 <- floor(x = activity$x_2)
-
-  activity$x_2 <- as.Date(x = activity$x_2,
-                          origin = "1899-12-30")
-
-  names(x = activity)[names(x = activity) == "x_2"] <- "activity_date"
-
+  
+  activity$x_2 <- trunc(x = activity$x_2)
+  
+  activity$x_2 <- lubridate::as_date(x = activity$x_2,
+                                     origin = "1899-12-30")
+  
+  # activity
+  
   dplyr::select(.data = activity,
                 mrn,
-                provider_name,
-                type,
-                summary,
-                activity_date,
-                pp_ship)
+                activity_provider,
+                pp_ship,
+                activity_date = x_2,
+                activity_type,
+                activity_descr = x_1)
 
 }
