db5bd3ffcd042d025b06b522e35f1efad07d1eb0
zhaoy <zhaoy@users.noreply.github.com>
2020-12-13 10:15:59 -0500

    edit

--- R/path.R
+++ R/path.R
@@ -1,31 +1,28 @@
-#' @title Convert relative file-paths to absolute
+#' @title Convert relative file-paths to absolute file-paths
 #'
 #' @description
-#' Convert relative file-paths to absolute.
+#' Convert relative file-paths to absolute file-paths.
 #'
 #' @usage
-#' path(dirname, rpath)
+#' path(basename, rpath)
 #'
-#' @param dirname A directory above 1) the file / folder and 2) the R file.
-#' @param rpath Relative to \code{dirname}, path to the file / folder.
+#' @param basename A folder above the 1) target file / folder and 2) R-code file.
+#' @param rpath Relative to \code{basename}, path to the target file / folder. If all files / folders below \code{basename} are unique, name of the target file / folder is sufficient.
 #'
 #' @return
 #' A character vector.
 #'
-#' @importFrom fs path
-#' @importFrom rprojroot find_root has_basename
+#' @importFrom rprojroot find_root_file has_basename
 #'
 #' @export
 
-path <- function(dirname,
+path <- function(basename,
                  rpath) {
 
-  criterion <- rprojroot::has_basename(basename = dirname)
+  criterion <- rprojroot::has_basename(basename = basename)
 
-  root <- rprojroot::find_root(criterion = criterion,
-                               path = ".")
-
-  fs::path(root,
-           rpath)
+  rprojroot::find_root_file(rpath,
+                            criterion = criterion,
+                            path = ".")
 
 }
