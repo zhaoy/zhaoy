65df02245223b46705e6541e4f7c14f122db3521
zhaoy <zhaoy@users.noreply.github.com>
2022-08-11 16:31:29 -0400

    edit

---
+++ R/date_time.R
@@ -0,0 +1,52 @@
+#' @title Parse dates-times.
+#'
+#' @description
+#' Parse dates-times.
+#'
+#' @usage
+#' date_time(x, length, orders)
+#'
+#' @param x A vector.
+#' @param length Length.
+#' @param orders Orders.
+#'
+#' @return
+#' A date-time vector.
+#'
+#' @importFrom lubridate as_date parse_date_time
+#' @importFrom stringr str_length
+#'
+#' @export
+#'
+#' @examples
+#' x <- c("2000-1-1", "12345")
+#' date_time(x = x, length = 5, orders = "ymd")
+
+date_time <- function(x,
+                      length,
+                      orders) {
+
+  x_numeric <- x[! is.na(x = x) &
+                 stringr::str_length(string = x) == length]
+
+  if (length(x = x_numeric) >= 1) {
+
+    x[! is.na(x = x) &
+      stringr::str_length(string = x) == length] <- x |>
+        subset(subset = ! is.na(x = x) &
+                        stringr::str_length(string = x) == length) |>
+        as.numeric() |>
+        lubridate::as_date(origin = "1899-12-30") |>
+        as.character()
+
+  }
+
+  lubridate::parse_date_time(x = x,
+                             orders = orders,
+                             truncated = 0,
+                             quiet = TRUE,
+                             exact = FALSE,
+                             train = TRUE,
+                             drop = FALSE)
+
+}
