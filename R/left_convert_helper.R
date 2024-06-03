979cef927fb90b508de03f7a45b5cb1f5fe7a01c
zhaoy <zhaoy@users.noreply.github.com>
2024-06-02 22:45:01 -0400

    Add files via upload

---
+++ R/left_convert_helper.R
@@ -0,0 +1,55 @@
+#' @title 
+#' Convert data by left-joining data-frames and look-up tables.
+#'
+#' @description
+#' Convert data by left-joining data-frames and look-up tables.
+#'
+#' @usage
+#' left_convert_helper(x_df, x_var, y_df, y_var)
+#'
+#' @param x_df Left-hand-side data-frame.
+#' @param x_var Left-hand-side variable.
+#' @param y_df Right-hand-side data-frame.
+#' @param y_var Right-hand-side variable.
+#'
+#' @return
+#' A tibble.
+#'
+#' @importFrom dplyr filter join_by left_join pull rename select
+
+left_convert_helper <- function(x_df,
+                                x_var,
+                                y_df,
+                                y_var) {
+  
+  y_df <- dplyr::filter(.data = y_df,
+                        y_df[, 1] == y_var)
+  
+  x_df <- dplyr::select(.data = x_df,
+                        {{ x_var }})
+  
+  var_name <- names(x = x_df)
+  
+  x_df <- dplyr::rename(.data = x_df,
+                        raw = {{ x_var }})
+  
+  x_df <- dplyr::left_join(x = x_df,
+                           y = y_df,
+                           by = dplyr::join_by("raw"),
+                           keep = NULL,
+                           multiple = "all",
+                           unmatched = "drop",
+                           relationship = "many-to-one")
+  
+  x_df$raw[! is.na(x = x_df$analyzable)] <- x_df$analyzable[! is.na(x = x_df$analyzable)]
+  
+  x_df$raw[! is.na(x = x_df$analyzable) &
+           x_df$analyzable == "convert to missing"] <- NA
+  
+  names(x = x_df)[! is.na(x = names(x = x_df)) &
+                  names(x = x_df) == "raw"] <- var_name
+  
+  dplyr::pull(.data = x_df,
+              var = {{ x_var }})
+  
+}
