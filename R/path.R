c937768cc9e894ff1bb36ab62202b81be0e30d8f
zhaoy <zhaoy@users.noreply.github.com>
2024-06-02 21:20:00 -0400

    Add files via upload

--- R/path.R
+++ R/path.R
@@ -1,13 +1,13 @@
 #' @title Convert relative paths to absolute paths
 #'
 #' @description
-#' If the working directory is the target folder or below, convert relative paths to absolute paths.
+#' If the working directory is the root directory or below the root directory, convert a relative path to an absolute path.
 #'
 #' @usage
 #' path(basename, ...)
 #'
-#' @param basename The target folder.
-#' @param ... Relative to \code{basename}, path to the target file.
+#' @param basename Root directory.
+#' @param ... Relative to \code{basename}, path to the file or folder.
 #'
 #' @return
 #' A character vector.
@@ -18,13 +18,13 @@
 
 path <- function(basename,
                  ...) {
-
+  
   criterion <- rprojroot::has_basename(basename = basename)
-
+  
   path <- rprojroot::find_root_file(...,
                                     criterion = criterion,
                                     path = ".")
-
+  
   as.character(x = path)
-
+  
 }
