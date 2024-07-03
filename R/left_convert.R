61638e8b4d88ee6bd7922c6dd78c00f70c9ab4cc
zhaoy <zhaoy@users.noreply.github.com>
2024-07-03 17:00:14 -0400

    Add files via upload

--- R/left_convert.R
+++ R/left_convert.R
@@ -2,15 +2,15 @@
 #' Convert data by left-joining data-frames and look-up tables
 #'
 #' @description
-#' Convert data by left-joining data-frames and look-up tables.
+#' Convert data by left-joining a data-frame and a look-up table.
 #'
 #' @usage
 #' left_convert(x_df, x_var, y_df, category)
 #'
-#' @param x_df Left-hand-side data-frame.
-#' @param x_var Left-hand-side variable(s).
-#' @param y_df Right-hand-side data-frame.
-#' @param category Right-hand-side data-frame's category.
+#' @param x_df Left-side data-frame.
+#' @param x_var Left-side variable(s).
+#' @param y_df Right-side data-frame.
+#' @param category Right-side data-frame's category.
 #'
 #' @return
 #' A data-frame.
