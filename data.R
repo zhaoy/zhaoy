fa3c0719c015b0e7f1e9b73bca3e44378d66534c
zhaoy <zhaoy@users.noreply.github.com>
2024-11-11 18:26:52 -0500

    Add files via upload

---
+++ data.R
@@ -0,0 +1,17 @@
+#' Look-up table for converting raw data to analyzable data
+#'
+#' Look-up table for converting raw data to analyzable data, excluding
+#' sub-national location names.
+#'
+#' @format ## `look_up`
+#'
+#'   A data-frame:
+#' \describe{
+#'   \item{lu_category}{Category.}
+#'   \item{raw}{Raw data.}
+#'   \item{analyzable}{Analyzable data.}
+#' }
+#'
+#' @source TCC GWEP
+#' 
+"look_up"
---
+++ path.R
@@ -0,0 +1,26 @@
+#' @title Convert relative paths to absolute paths
+#'
+#' @description If the working directory is below the root directory, convert a
+#' relative path to an absolute path.
+#'
+#' @usage path(basename, ...)
+#'
+#' @param basename Root directory.
+#' @param ... Relative to \code{basename}, path to the file or folder.
+#'
+#' @return A vector.
+#'
+#' @importFrom rprojroot find_root_file has_basename
+#'
+#' @export
+
+path <- function(basename,
+                 ...) {
+  
+  criterion <- rprojroot::has_basename(basename = basename)
+  
+  path <- rprojroot::find_root_file(...,
+                                    criterion = criterion,
+                                    path = ".")
+  
+}
